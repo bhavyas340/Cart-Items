547b06dc66ca34608dcc752cc9bd1ccf77144379
Bhavya Sharma <101468605+bhavyas340@users.noreply.github.com>
2024-03-10 06:29:42 +0000

    new of 2024

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import Cart  from './cart';
-import Navbar from './Navbar';
+import React from "react";
+import Cart from "./cart";
+import Navbar from "./Navbar";
 // import * as firebase from 'firebase';
-import firebase from 'firebase/compat/app';
-import 'firebase/auth';
-import { doc, Firestore } from 'firebase/firestore';
+import firebase from "firebase/compat/app";
+import "firebase/auth";
+import { doc, Firestore } from "firebase/firestore";
 
 class App extends React.Component {
-  constructor (){
+  constructor() {
     // boz itis a java script thing we have to call the "super()" as a constructor of parent class that is "React.Component"
     super();
-    this.state = { // this.state is a object
-        products:[
+    this.state = {
+      // this.state is a object
+      products: [
         //     {
         //     price: 39999,
         //     title: 'Watch',
@@ -33,65 +34,63 @@ class App extends React.Component {
         //     img: 'https://images.macrumors.com/t/k2uQkkz6aPOLMDkWVTn-8tXXg2g=/1600x1200/smart/article-new/2022/02/13-inch-macbook-pro-m2-mock-feature-2.jpg',
         //     id: 3
         // }
-
-        ],
-        loading: true,
-        user:[]   
-    }
-    this.db= firebase.firestore();
+      ],
+      loading: true,
+      user: [],
+    };
+    this.db = firebase.firestore();
     // this.testing();
     // other was to use it by using .bind(this);
     // this.increaseQuantity = this.increaseQuantity.bind(this)
-}
+  }
 
-//
-componentDidMount (){
-  // firebase
-  // .firestore()
-  // .collection('products')
-  // .get()
-  // .then((snapshot) => {
-  //   console.log(snapshot);
+  //
+  componentDidMount() {
+    // firebase
+    // .firestore()
+    // .collection('products')
+    // .get()
+    // .then((snapshot) => {
+    //   console.log(snapshot);
 
-  //   snapshot.docs.map((doc) =>{
-  //     console.log(doc.data())
-  //   })
-  //   const products =snapshot.docs.map((doc)=>{
-  //     const data = doc.data();
+    //   snapshot.docs.map((doc) =>{
+    //     console.log(doc.data())
+    //   })
+    //   const products =snapshot.docs.map((doc)=>{
+    //     const data = doc.data();
 
-  //     data['id'] = doc.id;
-  //     return data;    //give object
-  //   })
+    //     data['id'] = doc.id;
+    //     return data;    //give object
+    //   })
 
-  //   this.setState({
-  //     products,
-  //     loading:false
-  //   })
-  // })
-  // to make update while firebase database update(Same time)
-  this.db
-  .collection('products')
-  .onSnapshot((snapshot) => {           // using this onSnapshot function this is possible real time update on sever while firebase update
-    console.log(snapshot);
+    //   this.setState({
+    //     products,
+    //     loading:false
+    //   })
+    // })
+    // to make update while firebase database update(Same time)
+    this.db.collection("products").onSnapshot((snapshot) => {
+      // using this onSnapshot function this is possible real time update on sever while firebase update
+      console.log(snapshot);
 
-    snapshot.docs.map((doc) =>{
-      console.log(doc.data())
-    })
-    const products =snapshot.docs.map((doc)=>{
-      const data = doc.data();
+      snapshot.docs.map((doc) => {
+        console.log(doc.data());
+      });
+      const products = snapshot.docs.map((doc) => {
+        const data = doc.data();
 
-      data['id'] = doc.id;
-      return data;    //give object
-    })
+        data["id"] = doc.id;
+        return data; //give object
+      });
 
-    this.setState({
-      products,
-      loading:false
-    })
-  })
-}
-handleIncreaseQuantity = (product) =>{
-    const {products} = this.state;
+      this.setState({
+        products,
+        loading: false,
+      });
+    });
+  }
+  handleIncreaseQuantity = (product) => {
+    const { products } = this.state;
     const index = products.indexOf(product);
 
     // products[index].Qty += 1;
@@ -99,133 +98,146 @@ handleIncreaseQuantity = (product) =>{
     //     products
     // })
 
-    const docRef = this.db.collection('products').doc(products[index].id);
+    const docRef = this.db.collection("products").doc(products[index].id);
 
     docRef
       .update({
-        Qty:products[index].Qty+1
+        Qty: products[index].Qty + 1,
       })
-      .then(() =>{
-        console.log('Updated Successfully')
+      .then(() => {
+        console.log("Updated Successfully");
       })
-      .catch((error) =>{
-        console.log("Error: ", error)
-      })
-
-}
-handleDecreaseQuantity = (product) =>{
-    const {products} = this.state;
+      .catch((error) => {
+        console.log("Error: ", error);
+      });
+  };
+  handleDecreaseQuantity = (product) => {
+    const { products } = this.state;
     const index = products.indexOf(product);
-    if(products[index].Qty === 0){
-        return;
+    if (products[index].Qty === 0) {
+      return;
     }
 
     // products[index].Qty -= 1;
     // this.setState({
     //     products
     // })
-    const docRef = this.db.collection('products').doc(products[index].id);
+    const docRef = this.db.collection("products").doc(products[index].id);
 
     docRef
-       .update({
-        Qty:products[index].Qty-1
-       })
-       .then(()=>{
-        console.log("Successfully decrease Qty");
-      }).catch((error)=>{
-        console.log("Error: ", error);
+      .update({
+        Qty: products[index].Qty - 1,
       })
-}
-handleDeleteProduct = (id) => {
-  const { products } = this.state;
+      .then(() => {
+        console.log("Successfully decrease Qty");
+      })
+      .catch((error) => {
+        console.log("Error: ", error);
+      });
+  };
+  handleDeleteProduct = (id) => {
+    const { products } = this.state;
 
-  // const items = products.filter((item) => item.id !== id); // [{}]
+    // const items = products.filter((item) => item.id !== id); // [{}]
 
-  // this.setState({
-  //   products: items
-  // })
-  const docRef = this.db.collection('products').doc(id);
+    // this.setState({
+    //   products: items
+    // })
+    const docRef = this.db.collection("products").doc(id);
 
-  docRef
-  .delete()
-  .then(()=>{
-    console.log("delete Successfully");
-  }).catch((error)=>{
-    console.log("Error: ", error);
-  })
-}
+    docRef
+      .delete()
+      .then(() => {
+        console.log("delete Successfully");
+      })
+      .catch((error) => {
+        console.log("Error: ", error);
+      });
+  };
 
-
-getCartCount = () =>{
-  const { products} = this.state;
-  let count =0 ;
-   products.forEach((product)=>{
-    count += product.Qty;
-   })
-   return count;
-}
-getCartTotal = () =>{
-  const {products} = this.state;
-   let cartTotal = 0;
-   products.map((product) =>{
-    if(product.Qty >0){
-    cartTotal = cartTotal+ product.Qty*product.price
-    }
-    return '';
-   })
-   return cartTotal;
-}
-addProduct=()=>{
-  this.db
-  .collection('products')
-  .add({
-    img:'',
-    price: 900,
-    Qty: 2,
-    title: "Washing machine",
-  })
-  .then((docRef)=>{     //docRef is a objects of add(ing,price,Qty etc.)
-    console.log('product have been added',docRef);
-  })
-  .catch((error)=>{
-    console.log("Error" ,error);
-  })
-}
-render () {
-  const { products, loading } = this.state;
-  return (
-    <div className="App">
-      <Navbar count={this.getCartCount()} />
-      <button onClick={this.addProduct} style={{padding:10 ,fontSize:20}}>Add a product</button>
-      <Cart
-        products={products}
-        onIncreaseQuantity={this.handleIncreaseQuantity}
-        onDecreaseQuantity={this.handleDecreaseQuantity}
-        onhandleDeleteProduct={this.handleDeleteProduct}
-      />
-      {loading && <><img src='https://boldist.co/wp-content/uploads/2021/04/Loading-Spinners-are-UX-Killers.gif' style={styles.img}></img><h1>Loading products ...</h1></>}
-      <div style={styles.div}><strong>TOTAL: { this.getCartTotal()}</strong></div>
-    </div>
-  );
-}
-}
-const styles ={
-  div:{
-    padding:10,
-    fontSize:20,
-    
-    fontFamily: 'sans-serif',
-    color:'white',
-    backgroundColor: '#e9a982',
-    height: 50,
-    display:'flex',
-    justifyContent: 'flex-start',
-    alignItems: 'center'
-  },
-  img:{
-    width:1700,
-    height:800
+  getCartCount = () => {
+    const { products } = this.state;
+    let count = 0;
+    products.forEach((product) => {
+      count += product.Qty;
+    });
+    return count;
+  };
+  getCartTotal = () => {
+    const { products } = this.state;
+    let cartTotal = 0;
+    products.map((product) => {
+      if (product.Qty > 0) {
+        cartTotal = cartTotal + product.Qty * product.price;
+      }
+      return "";
+    });
+    return cartTotal;
+  };
+  addProduct = () => {
+    this.db
+      .collection("products")
+      .add({
+        img: "",
+        price: 900,
+        Qty: 2,
+        title: "Washing machine",
+      })
+      .then((docRef) => {
+        //docRef is a objects of add(ing,price,Qty etc.)
+        console.log("product have been added", docRef);
+      })
+      .catch((error) => {
+        console.log("Error", error);
+      });
+  };
+  render() {
+    const { products, loading } = this.state;
+    return (
+      <div className="App">
+        <Navbar count={this.getCartCount()} />
+        <button onClick={this.addProduct} style={{ padding: 10, fontSize: 20 }}>
+          Add a product
+        </button>
+        <Cart
+          products={products}
+          onIncreaseQuantity={this.handleIncreaseQuantity}
+          onDecreaseQuantity={this.handleDecreaseQuantity}
+          onhandleDeleteProduct={this.handleDeleteProduct}
+        />
+        {loading && (
+          <>
+            <img
+              src="https://boldist.co/wp-content/uploads/2021/04/Loading-Spinners-are-UX-Killers.gif"
+              style={styles.img}
+            ></img>
+            <h1>Loading products ...</h1>
+          </>
+        )}
+        <div style={styles.div}>
+          <strong>TOTAL: {this.getCartTotal()}</strong>
+        </div>
+      </div>
+    );
   }
 }
+const styles = {
+  div: {
+    padding: 10,
+    fontSize: 20,
+
+    fontFamily: "sans-serif",
+    color: "white",
+    backgroundColor: "#e9a982",
+    height: 50,
+    display: "flex",
+    justifyContent: "flex-start",
+    alignItems: "center",
+  },
+  img: {
+    width: 1700,
+    height: 800,
+  },
+};
 
 export default App;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,48 +1,61 @@
-import React from 'react';
+import React from "react";
 
-const Navbar =(props) =>{ // this is the way you can write functional Componant 
-   
-        return(
-            <div style={styles.nav}>
-                <div style={styles.cartIconContainer}>
-                    <img style={styles.cartIcon} src="https://www.seekpng.com/png/detail/134-1344280_add-items-to-cart-minimalist-shopping-cart.png" alt='cart-icon'/>
-                    <span style={styles.cartCount}>{props.count}</span>
-                </div>
-            </div>
-        )
-    
-}
+const Navbar = (props) => {
+  // this is the way you can write functional Componant
+
+  return (
+    <div style={styles.nav}>
+      <h3 style={styles.h3}>Cart Site</h3>
+      <div style={styles.cartIconContainer}>
+        <img
+          style={styles.cartIcon}
+          src="https://www.seekpng.com/png/detail/134-1344280_add-items-to-cart-minimalist-shopping-cart.png"
+          alt="cart-icon"
+        />
+        <span style={styles.cartCount}>{props.count}</span>
+      </div>
+    </div>
+  );
+};
 
 const styles = {
-    cartIcon:{
-        height:32,
-        marginRight:20,
-        borderRadius:'10px'
-    },
-    nav:{
-        height: 60,
-        background:'#c5edb3',
-        display: 'flex',
-        justifyContent: 'flex-end',
-        alignItems: 'center'
-    },
-    image:{
-        height:110,
-        width:110,
-        borderRadius:4,
-        background:'#ccc'
-    },
-    cartIconContainer: {
-        position: 'relative'
-    },
-    cartCount:{
-        backgroundColor: "yellow",
-        borderRadius: '50%',
-        padding:'4px 8px',
-        position: 'absolute',
-        right: 0,
-        top:-9
-
-    }
+  h3: {
+    display: "flex",
+    justifyContent: "flex-start",
+    marginLeft: "10px",
+    flot: "left",
+    rotateDirection: "30deg",
+  },
+  cartIcon: {
+    height: 32,
+    marginRight: 20,
+    borderRadius: "10px",
+    display: "flex",
+    justifyContent: "flex-start",
+  },
+  nav: {
+    height: 60,
+    background: "#c5edb3",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  image: {
+    height: 110,
+    width: 110,
+    borderRadius: 4,
+    background: "#ccc",
+  },
+  cartIconContainer: {
+    position: "relative",
+  },
+  cartCount: {
+    backgroundColor: "yellow",
+    borderRadius: "50%",
+    padding: "4px 8px",
+    position: "absolute",
+    right: 0,
+    top: -9,
+  },
 };
 export default Navbar;
